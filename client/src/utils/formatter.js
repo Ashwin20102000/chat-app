d18f3384217f4a423b409befe62eede747fe94d8
Ashwin J <63511472+Ashwin20102000@users.noreply.github.com>
2022-10-15 01:07:18 +0530

    Added formatter

---
+++ client/src/utils/formatter.js
@@ -0,0 +1,23 @@
+export const formatConversation = (conversations,contacts,id,selectedConversationIndex) => {
+  return conversations.map((conversation, index) => {
+    const recipients = conversation.recipients.map(recipient => {
+      const contact = contacts.find(contact => {
+        return contact.id === recipient
+      })
+      const name = (contact && contact.name) || recipient
+      return { id: recipient, name }
+    })
+
+    const messages = conversation.messages.map(message => {
+      const contact = contacts.find(contact => {
+        return contact.id === message.sender
+      })
+      const name = (contact && contact.name) || message.sender
+      const fromMe = id === message.sender
+      return { ...message, senderName: name, fromMe }
+    })
+    
+    const selected = index === selectedConversationIndex
+    return { ...conversation, messages, recipients, selected }
+  })
+}
